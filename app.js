5cdb95e3c3238b6e20b9abf6a7c4a11908ce0ed9
developerUmair <umairmak143@gmail.com>
2025-06-11 20:03:27 +0500

    new changes for that bug fixes

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import cookieParser from "cookie-parser";
 import express from "express";
 import authRouter from "./routes/auth.routes.js";
 import cors from "cors";
+import connectToDatabase from "./database/database.js";
+
+connectToDatabase();
 
 // Setup Express
 const app = express();
@@ -12,5 +15,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use("/api/v1/auth", authRouter);
+
+app.get('/health', async (req, res) => {
+  try {
+    await mongoose.connection.db.admin().ping();
+    res.send('✅ MongoDB is connected');
+  } catch (err) {
+    res.status(500).send('❌ MongoDB is NOT connected');
+  }
+});
+
 
 export default app;
