c5cc86344f82fbe54545f2d38d350ae9fd62950b
developerUmair <umairmak143@gmail.com>
2025-06-11 18:48:53 +0500

    initial commit

---
+++ database/database.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import { DB_URI, NODE_ENV } from "../config/env.js";
+import process from "process";
+
+
+if (!DB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI variable inside .env.<development/production>.local"
+  );
+}
+
+
+const connectToDatabase = async () => {
+    try {
+        await mongoose.connect(DB_URI);
+            console.log(`Connected to database ${NODE_ENV} mode`);
+    } catch (error) {
+        console.error(`Error connecting database`, error)
+        process.exit(1)
+    }
+};
+
+
+export default connectToDatabase;
